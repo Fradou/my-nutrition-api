1fc3070de73d1d8d30dceabe0a4650f6612b6a79
afradet <alex.fradet.1@gmail.com>
2018-03-07 13:06:31 +0100

    Manage authentification and session cookie for rest api part.

--- src/main/java/com/fradou/nutrition/config/SpringAppSecurityApiConfig.java
+++ src/main/java/com/fradou/nutrition/config/SpringAppSecurityApiConfig.java
@@ -6,16 +6,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import com.fradou.nutrition.security.api.AuthenticationApiEntryPoint;
+
 @Configuration
-@Order(2)
+@EnableWebSecurity
+@Order(1)
 public class SpringAppSecurityApiConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	@Qualifier("customUserDetailsService")
 	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private AuthenticationApiEntryPoint authenticationApiEntryPoint;
 	
 	/**
 	 * Use custom userProvider
@@ -32,20 +39,21 @@ public class SpringAppSecurityApiConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeRequests()
-			.antMatchers("/api/**").permitAll()
+		http
+			.csrf().disable()
+			.exceptionHandling()
+			.authenticationEntryPoint(authenticationApiEntryPoint)
+			
+			.and()
+			.authorizeRequests()
+			.antMatchers("/api/**")
+			.authenticated()
+			
 			.and()
 			.formLogin()
-				// URL to custom login form
-				.loginPage("/login")
-				// URL to check login
-				.loginProcessingUrl("/authenticateUser")
-			//	.defaultSuccessUrl(defaultSuccessUrl)
-				// Allow everyone to see login page
-				.permitAll()
+			
 			.and()
-				// Enable standard logout
-				.logout()
-				.permitAll();
+			.logout()
+			.permitAll();
 	}
 }
--- src/main/java/com/fradou/nutrition/config/SpringAppSecurityConfig.java
+++ src/main/java/com/fradou/nutrition/config/SpringAppSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -36,10 +37,11 @@ public class SpringAppSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeRequests()
-			.antMatchers("/resources/**").permitAll()
+		http
+			.authorizeRequests()
 			.antMatchers("/register").permitAll()
 			.antMatchers("/**").authenticated()
+			
 			.and()
 			.formLogin()
 				// URL to custom login form
@@ -49,12 +51,21 @@ public class SpringAppSecurityConfig extends WebSecurityConfigurerAdapter {
 			//	.defaultSuccessUrl(defaultSuccessUrl)
 				// Allow everyone to see login page
 				.permitAll()
+			
 			.and()
 				// Enable standard logout
 				.logout()
 				.permitAll()
+			
 			.and()
 				.exceptionHandling()
 				.accessDeniedPage("/access-denied");
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+			.ignoring()
+			.antMatchers("/resources/**");		
 	}
 }
